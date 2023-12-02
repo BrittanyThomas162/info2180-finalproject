371e5fcea439f1b4daf40afb0d5fcf3c424ae7a9
Brittany Thomas <yaneelthomas162@gmail.com>
2023-12-02 16:04:07 -0500

    Added link for homepage

--- menu.php
+++ menu.php
@@ -1,8 +1,8 @@
 <nav>
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">New Contact</a></li>
-          <li><a href="#">Users</a></li>
-          <li><a href="#">Logout</a></li>
-        </ul>
-      </nav>
+  <ul>
+    <li><a href="dashboard.php">Home</a></li>
+    <li><a href="#">New Contact</a></li>
+    <li><a href="#">Users</a></li>
+    <li><a href="#">Logout</a></li>
+  </ul>
+</nav>
