36386009ae5e61d68729d4fd4e2268c01c7707dc
Brianna Roper <133825144+ria876@users.noreply.github.com>
2023-12-07 10:03:16 -0500

    Update assigntome.php

--- assigntome.php
+++ assigntome.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-$_SESSION['user_id'] = '1';       
+$_SESSION['id'] = '1';       
 
 $host = 'localhost';
 $username = 'user123';
@@ -17,12 +17,12 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $contactId = $_POST['id'];
 
 // Get the ID of the logged-in user
-$userId = $_SESSION['user_id'];
+$userId = $_SESSION['id'];
 
 // Update the assigned_to field in the contacts table
-$stmt = $conn->prepare("UPDATE contacts SET assigned_to = :user_id WHERE id = id");
-$stmt = $conn->prepare("UPDATE contacts SET assigned_to = :user_id, updated_at = NOW() WHERE id = :contact_id");
-$stmt->execute(['user_id' => $userId, 'contact_id' => $contactId]);
-$stmt->execute(['user_id' => $userId, 'contact_id' => $contactId]);
+ $stmt = $conn->prepare("UPDATE contacts SET assigned_to = :id WHERE id = contact_id");
+ $stmt = $conn->prepare("UPDATE contacts SET assigned_to = :id, updated_at = NOW() WHERE id = :contact_id");
+
+$stmt->execute(['id' => $userId, 'contact_id' => $contactId]);
 
 ?>
