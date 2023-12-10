1f8d674756048e108b564402d03392ea985343ea
Anisah02 <118861260+Anisah02@users.noreply.github.com>
2023-12-10 13:20:34 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,43 +1,5 @@
 <?php
 session_start();
-
-$host = "localhost";
-$username = "admin";
-$password = "password123";
-$dbname = "dolphin_crm";
-$conn = mysqli_connect($host, $username, $password, $dbname);
-
-if (!$conn) {
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-
-if (isset($_POST["login"])) {
-    $email = $_POST["email"];
-    $pwd = $_POST["password"];
-
-    $sql = "SELECT * FROM users WHERE email = ?";
-    $stmt = mysqli_prepare($conn, $sql);
-    mysqli_stmt_bind_param($stmt, 's', $email);
-    mysqli_stmt_execute($stmt);
-    $result = mysqli_stmt_get_result($stmt);
-    $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
-
-    if ($user) {
-        if (password_verify($pwd, $user["password"]) || ($email == 'admin@project2.com' && $pwd == 'password123')) {
-            $_SESSION["user_id"] = $user["id"];
-            $_SESSION["user_email"] = $user["email"];
-            $_SESSION["role"] = $user["role"];
-            header("Location: dashboard.php");
-            exit;
-        } else {
-            echo "Incorrect password!!";
-        }
-    } else {
-        echo "Email address not found!!";
-    }
-
-    mysqli_stmt_close($stmt);
-}
 ?>
 
 <!DOCTYPE html>
@@ -50,55 +12,49 @@ if (isset($_POST["login"])) {
     <link rel="stylesheet" href="login.css">
 </head>
 
+    <h2>Login</h2>
 <body>
     <?php
     $host = "localhost";
-    $username = "admin";
-    $password = "password123";
+    $username = "root";
+    $password = "";
     $dbname = "dolphin_crm";
-    $conn = mysqli_connect($host, $username, $password, $dbname);
+    $conn= mysqli_connect($host,$username,$password,$dbname);
 
-    if (!$conn) {
-        die("ERROR: Could not connect. " . mysqli_connect_error());
-    }
-
-    if (isset($_POST["login"])) {
+    if (isset($_POST["login"])) 
+    {
         $email = $_POST["email"];
         $pwd = $_POST["password"];
+        $sql = "SELECT * FROM users WHERE email = '$email'";
+        $result = mysqli_query($conn, $sql);
+        $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
-        // Use prepared statement to prevent SQL injection
-        $sql = "SELECT * FROM users WHERE email = ?";
-        $stmt = mysqli_prepare($conn, $sql);
-        mysqli_stmt_bind_param($stmt, 's', $email);
-        mysqli_stmt_execute($stmt);
-        $result = mysqli_stmt_get_result($stmt);
-        $user = mysqli_fetch_assoc($result);
-
-        if ($user) {
-            if (password_verify($pwd, $user["password"]) || ($email == 'admin@project2.com' && $pwd == 'password123')) {
-                $_SESSION["user_id"] = $user["id"];
-                $_SESSION["user_email"] = $user["email"];
-                $_SESSION["role"] = $user["role"];
+        if ($user) 
+        {
+            if ($pwd== $user["password"]|| ($email == 'admin@project2.com' && $pwd == 'password123')) 
+            {
+                session_start();
+                $_SESSION["user_id"] = $row["id"];
+                $_SESSION["user_email"] = $row["email"];
                 header("Location: dashboard.php");
-                exit;
-            } else {
+                die();
+            }
+            else
+            {
                 echo "Incorrect password!!";
             }
-        } else {
+         }
+         else
+         {
             echo "Email address not found!!";
-        }
-
-        // Close the statement
-        mysqli_stmt_close($stmt);
+         }
     }
     ?>
-
     <form action="login.php" method="post">
-        <input type="text" name="email" placeholder="Email Address" required>
+        <input type="email" name="email" placeholder= "Email Address" required>
         <input type="password" name="password" placeholder="Password" required>
-        <button type="submit" id="login" name="login" value="Login">Login</button>
+        <button type="submit" name= "login" value="Login">Login</button>
     </form>
-
     <p id="copyright">Copyright © 2022 Dolphin CRM</p>
 </body>
 </html>
